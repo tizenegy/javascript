e6973f6a7009f9d9e1b19ed9f3bdef4fb0f25f17
Bastian Elfert <bastian.elfert@outlook.com>
2020-11-03 20:51:20 +0200

    very basic structure of a SPA

---
+++ sandbox/singlepage.js
@@ -0,0 +1,16 @@
+
+function showPage(page) {
+    document.querySelectorAll("div").forEach(div=>{
+        div.style.display = "none";
+    })
+    document.querySelector(`#${page}`).style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.querySelectorAll('button').forEach(button => {
+        button.onclick = function() {
+            showPage(this.dataset.page);
+            
+        }
+    });
+});
