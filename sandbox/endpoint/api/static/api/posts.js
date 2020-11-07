31ab5d83de3b8e2f45ccec68bfa22e6d36fb36f6
Bastian Elfert <bastian.elfert@outlook.com>
2020-11-07 18:37:38 +0200

    fixed referencing of posts.js from layout.html

--- sandbox/endpoint/api/static/api/posts.js
+++ sandbox/endpoint/api/static/api/posts.js
@@ -1,21 +1,15 @@
 
-// Start with first post
 let counter = 1;
+const quantity = 10;
 
-// Load posts 20 at a time
-const quantity = 20;
-
-// When DOM loads, render the first 20 posts
 document.addEventListener('DOMContentLoaded', load);
 
-// If scrolled to bottom, load the next 20 posts
 window.onscroll = () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         load();
     }
 };
 
-// If hide button is clicked, delete the post
 document.addEventListener('click', event => {
     const element = event.target;
     if (element.className === 'hide') {
@@ -26,15 +20,10 @@ document.addEventListener('click', event => {
     }
 });
 
-// Load next set of posts
 function load() {
-
-    // Set start and end post numbers, and update counter
     const start = counter;
     const end = start + quantity - 1;
     counter = end + 1;
-
-    // Get new posts and add posts
     fetch(`/posts?start=${start}&end=${end}`)
     .then(response => response.json())
     .then(data => {
@@ -42,14 +31,9 @@ function load() {
     })
 };
 
-// Add a new post with given contents to DOM
 function add_post(contents) {
-
-    // Create new post
     const post = document.createElement('div');
     post.className = 'post';
     post.innerHTML = `${contents} <button class="hide">Hide</button>`;
-
-    // Add post to DOM
     document.querySelector('#posts').append(post);
 };
